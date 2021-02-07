d01217e54863334747970863debc7a938c0b2780
danayang1120 <danay470@gmail.com>
2021-02-07 13:05:24 -0500

    added exit button, working scale

--- app/src/main/java/com/example/radnorapp/Feelings.java
+++ app/src/main/java/com/example/radnorapp/Feelings.java
@@ -13,64 +13,50 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class Feelings extends AppCompatActivity {
+public class Feelings extends AppCompatActivity implements View.OnClickListener {
 
-    Button feeling1;
-    Button feeling2;
-    Button feeling3;
-    Button feeling4;
-    Button feeling5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button feeling1;
+        Button feeling2;
+        Button feeling3;
+        Button feeling4;
+        Button feeling5;
+        Button exitButton2;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feelings);
 
-        // feeling scale 1
         feeling1 = findViewById(R.id.feelingOne);
-        feeling1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent clickOne = new Intent(getApplicationContext(), QuotesActivity.class);
-                startActivity(clickOne);
-            }
-        });
-
-        // feeling scale 2
         feeling2 = findViewById(R.id.feelingTwo);
-        feeling2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent clickTwo = new Intent(getApplicationContext(), QuotesActivity.class);
-                startActivity(clickTwo);
-            }
-        });
-
-        // feeling scale 3
         feeling3 = findViewById(R.id.feelingThree);
-        feeling3.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent clickThree = new Intent(getApplicationContext(), QuotesActivity.class);
-                startActivity(clickThree);
-            }
-        });
-         // feeling scale 4
         feeling4 = findViewById(R.id.feelingFour);
-        feeling4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent clickFour = new Intent(getApplicationContext(), QuotesActivity.class);
-                startActivity(clickFour);
-            }
-        });
-
-        // feeling scale 5
         feeling5 = findViewById(R.id.feelingFive);
-        feeling5.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent clickFive = new Intent(getApplicationContext(), QuotesActivity.class);
-                startActivity(clickFive);
-            }
-        });
+        exitButton2 = findViewById(R.id.exitButton2);
+
+        feeling1.setOnClickListener(this);
+        feeling2.setOnClickListener(this);
+        feeling3.setOnClickListener(this);
+        feeling4.setOnClickListener(this);
+        feeling5.setOnClickListener(this);
+        exitButton2.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.exitButton2){
+            Intent exit = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(exit);
+        }
+        else {
+            Intent click = new Intent(getApplicationContext(), QuotesActivity.class);
+            startActivity(click);
+        }
+
+    }
 
 
 
--- app/src/main/java/com/example/radnorapp/QuotesActivity.java
+++ app/src/main/java/com/example/radnorapp/QuotesActivity.java
@@ -2,13 +2,26 @@ package com.example.radnorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.widget.Button;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuotesActivity extends AppCompatActivity {
+
+    Button exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotes);
+
+        exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent exit = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(exit);
+            }
+        });
     }
 }
